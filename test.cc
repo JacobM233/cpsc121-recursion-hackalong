7fdc27d673b2dbd319c511ad5fb85b642695d34b
JacobM233 <JacobMijares@csu.fullerton.edu>
2026-04-09 19:45:49 +0000

    done

--- test.cc
+++ test.cc
@@ -1,1 +1,25 @@
 // Jacob Mijares
+// 838781508
+// 12 PM Section 13
+// Hack-a-long worksheet 21
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int sumVector(int index, const std::vector<int>& vec) {
+ if (index == vec.size()) {
+    return 0;
+ } else {
+    int temp1 = vec.at(index);
+    int temp2 = sumVector(index + 1, vec);
+    return temp1 + temp2;
+ }
+} 
+
+int main() {
+ std::vector<int> myVector = {3, 5, 7};
+ int finalTotal = sumVector(0, myVector);
+ std::cout << "The sum of the vector is " << finalTotal << "\n";
+ return 0;
+}
